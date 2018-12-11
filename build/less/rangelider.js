c0224a046ece89d8bc429ee44dda52ea50e1f0b2
Valentyn Molodyk <v.molodyk@x2sy.com>
2018-12-11 15:21:58 +0200

    ddd

--- build/less/rangelider.js
+++
@@ -1,7 +0,0 @@
-$('input[type="range"]').val(10).change();
-
-&('input[type="range"]').rangeslider();
-
-&('input[type="range"]').rangeslider('destroy');
-
-&('input[type="range"]').rangeslider('update', true);
